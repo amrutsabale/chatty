0773faba122e0e63953386e3b7dbb091aa512d36
amrutsabale <samrut355@gmail.com>
2022-09-27 20:54:17 +0530

    Created static Register component with styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Chat />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
